04054e583d07e52e5159bf1bf6dd52643128c34f
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-08-05 15:31:47 -0300

    ej3 terminado

--- Ej3.cpp
+++ Ej3.cpp
@@ -1,9 +1,34 @@
+/*Escribe un programa en C++ que tenga una función llamada marcarAsiento que
+tome un puntero a un booleano y lo cambie a true para indicar que el asiento está
+reservado. Luego, en la función main, crea un arreglo que represente los asientos
+del teatro (inicialmente todos libres), pide al usuario que elija un asiento para
+reservar, llama a la función marcarAsiento y muestra el estado de los asientos antes
+y después de la reserva.*/
+
 #include <iostream>
 using namespace std;
+void marcarAsiento(bool *a);
 
 int main() {
+    const int n=10;
+    bool asientos[n]={false};
+    int asientoElegido;
 
-    cout << "Examen Final Prog 3" << endl;
+    cout << "Estado inicial de los asientos:\n";
+    for (int i = 0; i <n; ++i) {
+        cout << "Asiento " << i + 1 << ": " << (asientos[i] ? "Reservado" : "Libre") << "\n";
+    }
+    cout << "Elija un asiento para reservar (1-" << n << "): ";
+    cin >> asientoElegido;
+    marcarAsiento(&asientos[asientoElegido - 1]);
+
+    cout << "Estado de los asientos después de la reserva:\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "Asiento " << i + 1 << ": " << (asientos[i] ? "Reservado" : "Libre") << "\n";
+    }
 
     return 0;
+}
+void marcarAsiento(bool *a){
+    *a=true;
 }
