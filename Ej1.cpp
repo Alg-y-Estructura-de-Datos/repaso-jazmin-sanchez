38c0f45950ab2f5b249a3c492259029ebeb3a356
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-08-05 14:08:48 -0300

    ejercicio1 terminado

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void vasos(float &v1, float &v2);
+
 int main() {
+    float v1, v2;
+    cout << "ingrese cantidad de jugo Manzana: " << endl;
+    cin>>v1;
+    cout << "ingrese cantidad de jugo Naranja: " << endl;
+    cin>>v2;
+    cout<<"NARANJA:"<<v2<<endl;
+    cout<<"MANZANA:"<<v1<<endl;
 
-    cout << "Examen Final Prog 3" << endl;
-
+    vasos(v1,v2);
+    cout<<"NARANJA:"<<v2<<endl;
+    cout<<"MANZANA:"<<v1<<endl;
     return 0;
+}
+
+void vasos(float &v1, float &v2){
+    float a= v1;
+    v1=v2;
+    v2=a;
 }
